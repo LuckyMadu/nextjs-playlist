e993cf2af14799e527d249ea84045139230a5bf0
Lahiru Amaratunge <lahiru.a@keeneye.solutions>
2021-12-19 10:39:16 +0530

    button navigate

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/product");
+  };
   return (
     <div>
       <h1>Home page</h1>
@@ -10,6 +16,8 @@ export default function Home() {
       <Link href="/product">
         <a>Products</a>
       </Link>
+
+      <button onClick={handleClick}>Order Product</button>
     </div>
   );
 }
