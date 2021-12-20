cf39373c23d8d9226cc10dad438c91a6a36bf458
Lahiru Amaratunge <lahiru.a@keeneye.solutions>
2021-12-20 09:25:41 +0530

    get static path fallback false

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -11,6 +11,43 @@ function Post({ post }) {
 
 export default Post;
 
+// if fallback false, havnt any path returned by getStaticPaths, will return 404
+export async function getStaticPaths() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  const paths = data.map((post) => {
+    return {
+      params: {
+        //convert to string
+        postId: `${post.id}`,
+      },
+    };
+  });
+
+  return {
+    // paths: [
+    //   {
+    //     params: {
+    //       postId: "1",
+    //     },
+    //   },
+    //   {
+    //     params: {
+    //       postId: "2",
+    //     },
+    //   },
+    //   {
+    //     params: {
+    //       postId: "3",
+    //     },
+    //   },
+    // ],
+    paths,
+    fallback: false,
+  };
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
   const response = await fetch(
