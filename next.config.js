c7b65a18fab4b3c021655214e51cbb070f52fc7f
Lahiru Amaratunge <lahiru.a@keeneye.solutions>
2022-01-19 19:00:38 +0530

    env variables

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
-}
+  redirects: async () => {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/old/:id",
+        destination: "/new/:id",
+        permanent: true,
+      },
+    ];
+  },
+};
--- pages/blog/[blogId].js
+++ pages/blog/[blogId].js
@@ -7,12 +7,15 @@ export default function Blog({ title, desc }) {
         <title>{title}</title>
         <meta name="description" content={desc} />
         <h1>Blog</h1>
+        <h2>ID: {process.env.NEXT_PUBLIC_ID}</h2>
       </Head>
     </>
   );
 }
 
 export async function getServerSideProps() {
+  const user = process.env.DB_USER;
+  console.log("user", user);
   return {
     props: {
       title: "Blog",
