e3e29ccc21ad9fc4c88dca1779c925a761937760
Lahiru Amaratunge <lahiru.a@keeneye.solutions>
2022-01-20 10:53:54 +0530

    secure server sde pages

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../components/Navbar";
 import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+function MyApp({ Component, pageProps }) {
   if (Component.getLayout) {
     return Component.getLayout(<Component {...pageProps} />);
   }
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <Navbar />
       <Component {...pageProps} />
     </SessionProvider>
--- pages/blog.jsx
+++ pages/blog.jsx
@@ -1,9 +1,30 @@
-function Blog() {
+import { getSession } from "next-auth/react";
+
+function Blog({ data }) {
   return (
     <>
-      <div>Blog</div>
+      <div>Blog: {data}</div>
     </>
   );
 }
 
 export default Blog;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: `/api/auth/signin?callbackUrl=http://localhost:3000/blog`,
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      session,
+      data: session ? "List of filtered posts" : "List of posts",
+    },
+  };
+}
